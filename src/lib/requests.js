06940da2bd5484ccdc77052e6e2628be4a610ae3
Reza <rezamahmudy1@gmail.com>
2022-08-15 14:20:06 +0430

    add redirect

--- src/lib/requests.js
+++ src/lib/requests.js
@@ -42,7 +42,7 @@ export const getProfile = async () => {
 export const getOrders = async ({ page = 14, size = 100 } = {}) => {
   loadingState.update(() => true);
   return await fetch(
-    `/order`,
+    `/order/reorder?page=${page}&size=${size}`,
     {
       headers: {
         authorization: localStorage.getItem("token"),
