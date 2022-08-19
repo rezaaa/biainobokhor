2cbe25ab7616ef2e180c92c65556074a20d08883
Reza <rezamahmudy1@gmail.com>
2022-08-19 15:48:26 +0430

    add gtag

--- src/lib/Loading.svelte
+++ src/lib/Loading.svelte
@@ -14,7 +14,7 @@
   <BarLoader size="60" color="#008ffd" unit="px" duration="1s" />
   <div>در حال دریافت سفارشات</div>
   <p>درصورت بالا بودن تعداد سفارشات، ممکن است این فرآیند کمی طول بکشد</p>
-  {#if orders?.currentPage > 0}
+  {#if orders?.currentPage + 1 > 0}
     <p>
       {`دریافت صفحه ${farsiNumber(orders?.currentPage + 1)} از ${farsiNumber(
         orders?.pages + 1
